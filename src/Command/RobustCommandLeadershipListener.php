eff16b1625b8cad817b5e69ef337adedb3c14540
danilo silva <danilo.silva@easywelfare.com>
2019-05-30 12:59:31 +0200

    Calling handler on command When leadership status changes

---
+++ src/Command/RobustCommandLeadershipListener.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Geezer\Command;
+
+interface LeadershipEventsHandler
+{
+    /**
+     * This method is called every time leadership status changes from lost to acquired.
+     */
+    public function leadershipAcquired(): void;
+
+    /**
+     * This method is called every time leadership status changes from acquired to lost.
+     */
+    public function leadershipLost(): void;
+}
