232cf7459bb0c11c0a53e91d948049c3fed004ef
Danilo Silva <badkill82@gmail.com>
2020-02-26 15:57:36 +0100

    exit with an error code in case of exception

--- src/Command/RobustCommandRunner.php
+++ src/Command/RobustCommandRunner.php
@@ -2,9 +2,9 @@
 
 namespace Recruiter\Geezer\Command;
 
-use Recruiter\Geezer\Timing\WaitStrategy;
-use Psr\Log\LogLevel;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Recruiter\Geezer\Timing\WaitStrategy;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -95,6 +95,10 @@ class RobustCommandRunner extends Command
         $this->wrapped->shutdown($occuredException);
 
         $leadershipStrategy->release();
+
+        if ($occuredException) {
+            exit(2);
+        }
     }
 
     private function leadershipWasAcquired()
